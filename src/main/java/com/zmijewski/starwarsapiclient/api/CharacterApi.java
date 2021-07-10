b55559321c660302e806994c5b6c8d3e8cd8c0a2
Adam Żmijewski <zmijewskiadam94@gmail.com>
2021-07-10 17:29:22 +0200

    Swagger added

--- src/main/java/com/zmijewski/starwarsapiclient/api/CharacterApi.java
+++ src/main/java/com/zmijewski/starwarsapiclient/api/CharacterApi.java
@@ -4,6 +4,10 @@ import com.zmijewski.starwarsapiclient.api.model.CharacterModel;
 import com.zmijewski.starwarsapiclient.api.model.PageModel;
 import com.zmijewski.starwarsapiclient.application.CharacterService;
 import com.zmijewski.starwarsapiclient.application.exceptions.EntityNotFoundException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -13,15 +17,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/characters")
 @RequiredArgsConstructor
+@Api("Characters")
 public class CharacterApi {
     private final CharacterService characterService;
 
     @GetMapping
+    @ApiOperation("Get characters page")
+    @ApiResponse(code = 200, message = "Page was received successfully")
     public PageModel<CharacterModel> getAll(@PageableDefault Pageable pageable) {
         return characterService.getAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Get character by his id")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Character was found"),
+            @ApiResponse(code = 404, message = "Characters with id not found")
+    })
     public CharacterModel getById(@PathVariable("id") Long id) {
         return characterService.getCharacterById(id);
     }
